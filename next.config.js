deafa9cc7a04624947bdcac48142ede1b3fe67b7
Muzaffar Mohammed <50576677+MuzaffarMohammed@users.noreply.github.com>
2021-04-12 13:42:08 +0530

    next.config changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+let env = {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL": "mongodb+srv://admin:admin123@kfmdatacluster.k12wf.mongodb.net/kfmData?retryWrites=true&w=majority",
+    "ACCESS_TOKEN_SECRET": "*C837NzAgSbc5$Xg-S#b?!fV@vs*R6GdF4Ny",
+    "REFRESH_TOKEN_SECRET": "EpBc_%qZY&CuX+7-q%9V!MR47xhFLW@Fk$3W",
+    "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID"
+}
+if (process.env.NODE_ENV === 'production') {
+   env = {}
+  }
+
 module.exports = {
-    env: {
-        "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "mongodb+srv://admin:admin123@kfmdatacluster.k12wf.mongodb.net/kfmData?retryWrites=true&w=majority",
-        "ACCESS_TOKEN_SECRET": "*C837NzAgSbc5$Xg-S#b?!fV@vs*R6GdF4Ny",
-        "REFRESH_TOKEN_SECRET": "EpBc_%qZY&CuX+7-q%9V!MR47xhFLW@Fk$3W",
-        "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID"
-    }
+    env:env
 }
