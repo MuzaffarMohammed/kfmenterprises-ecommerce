d9be11f7873697f0f916177fed6e0fff4a297c1a
Muzaffar Mohammed <50576677+MuzaffarMohammed@users.noreply.github.com>
2021-04-15 17:12:20 +0530

    minro changes

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,12 @@ class MyDocument extends Document{
         return(
             <Html lang="en">
                 <Head>
-                    <meta name="description" content="Dev AT E-commerce website with Next.js"/>
+                    <meta name="description" content="KFM Enterprises E-Commerce Website"/>
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
                     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-                    <script src="https://kit.fontawesome.com/a3140e7c2c.js" crossOrigin="anonymous"></script>                    <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}></script>
+                    <script src="https://kit.fontawesome.com/a3140e7c2c.js" crossOrigin="anonymous"></script>
+                    <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}></script>
                 </Head>
                 <body>
                     <Main />
