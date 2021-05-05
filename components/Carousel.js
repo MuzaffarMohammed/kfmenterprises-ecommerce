d0be3ff6668f454e878b343140b6926061e581c6
MohammedHabeebkhan <mdhabeebkhan11446@gmail.com>
2021-05-05 06:47:00 +0530

    carousel component code aligned properly

--- components/Carousel.js
+++ components/Carousel.js
@@ -11,7 +11,7 @@ function ControlledCarousel() {
 
   return (
     <Carousel>
-  <Carousel.Item interval={2500}>
+    <Carousel.Item interval={2500}>
     <img
       className="d-block w-100"
       src="/assets/images/carouselImage/honey.jpg"
@@ -21,30 +21,30 @@ function ControlledCarousel() {
       <h3>First slide label</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={2500}>
-    <img
+     </Carousel.Item>
+     <Carousel.Item interval={2500}>
+      <img
       className="d-block w-100"
       src="/assets/images/carouselImage/spice.jpg"
       alt="Second slide"
-    />
-    <Carousel.Caption>
+      />
+     <Carousel.Caption>
       <h3>Second slide label</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={2500}>
-    <img
+      </Carousel.Caption>
+     </Carousel.Item>
+     <Carousel.Item interval={2500}>
+     <img
       className="d-block w-100"
       src="/assets/images/carouselImage/potBiryani.jpg"
       alt="Third slide"
-    />
-    <Carousel.Caption>
+      />
+      <Carousel.Caption>
       <h3>Third slide label</h3>
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+     </Carousel.Caption>
+     </Carousel.Item>
+     </Carousel>
   );
 }
 
