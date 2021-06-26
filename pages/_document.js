477c8072f53fc40d38ab0d16cda00c0708df022f
MuzaffarMohammed <muzaffarmohammed43@gmail.com>
2021-06-26 12:35:59 +0530

    Jquery issue fix

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,8 @@ class MyDocument extends Document{
             <Html lang="en">
                 <Head>
                     <meta name="description" content="KFM Enterprises E-Commerce Website"/>
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
                     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
                     <script src="https://kit.fontawesome.com/a3140e7c2c.js" crossOrigin="anonymous"></script>
                 </Head>
--- pages/api/uploads/index.js
+++ pages/api/uploads/index.js
@@ -1,36 +1,6 @@
 import nextConnect from 'next-connect';
-import path from 'path';
-import multer from 'multer';
 import {cloud_uploads} from './cloudinary';
-
-const oneMegabyteInBytes = 1000000;
-
-const upload = multer({
-  limits: { fileSize: oneMegabyteInBytes * 2 },
-  storage: multer.diskStorage({
-    destination: './public/assets/images',
-    filename: (req, file, cb) => cb(null, file.originalname),
-  }),
-  fileFilter: function(req, file, cb){
-    checkFileType(file, cb);
-  }
-});
-
-// Check File Type
-function checkFileType(file, cb){
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
-
-  if(mimetype && extname){
-    return cb(null,true);
-  } else {
-    cb('Error: Images Only!');
-  }
-}
+import { upload } from "./multer";
 
 const apiRoute = nextConnect({
   onError(error, req, res) {
