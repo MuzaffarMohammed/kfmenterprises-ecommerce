2eb5cce2dc3f6927d16409b14f06b3e232c08313
MuzaffarMohammed <muzaffarmohammed43@gmail.com>
2021-07-31 16:17:16 +0530

    Conflict merged

--- pages/forgotPassword.js
+++ pages/forgotPassword.js
@@ -32,12 +32,12 @@ const ForgotPassword = () => {
 
     const res = await postData('auth/checkUserExist', userDataRecovery)
 
-    if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err }})
+    if (res.err) return dispatch({ type: 'NOTIFY', payload: { error: res.err } })
     if (res && res.user && !res.user.activated) return dispatch({ type: 'NOTIFY', payload: { error: 'Please activate your account to proceed further.' } })
-    
+
     const resMail = await postData('mail', { forgotPasswordUrl: process.env.NEXT_PUBLIC_BASE_URL + `/resetPassword?userid=${res.user.id}&ts=${moment().valueOf()}`, email: accountRecoveryEmail, mailType: PASSWORD_RESET_MAIL, subject: 'Password Reset Request', userName: res.user.name })
     if (resMail.err) return dispatch({ type: 'NOTIFY', payload: { error: "Sorry, something went wrong! Please try again." } })
-   
+
     return dispatch({ type: 'NOTIFY', payload: { success: "We have successfully sent a password reset mail to your verified email address. Please check your mail inbox." } })
   }
 
@@ -50,11 +50,11 @@ const ForgotPassword = () => {
       <form className="container-fluid mx-auto my-4 border_login" style={{ maxWidth: '500px' }} onSubmit={handleSubmit}>
         <h1>Find Your Account</h1>
 
-        <p>Please enter your email address or username to search for your account.</p>
+        <p>Please enter your email address.</p>
         <div className="form-group">
-          <label htmlFor="userName">User Name</label>
+          {/* <label htmlFor="userName">Email Address</label> */}
           <input type="email" className="form-control" id="exampleAccountRecoveryEmail" aria-describedby="emailHelp"
-            name="accountRecoveryEmail" value={accountRecoveryEmail} onChange={handleChangeInput} />
+            name="accountRecoveryEmail" value={accountRecoveryEmail} onChange={handleChangeInput} placeholder="example@gmail.com" />
         </div>
 
         <button type="submit" className="btn btn-dark signBtn w-100">Submit</button>
