037612fc58c640a2411dac7c865232b36fab7a1d
MuzaffarMohammed <muzaffarmohammed43@gmail.com>
2021-08-03 19:21:25 +0530

    Issue # 22 solved

--- utils/filterSearch.js
+++ utils/filterSearch.js
@@ -1,17 +1,17 @@
-const filterSearch = ({router, page, category, sort, search}) => {
+const filterSearch = ({ router, page, category, sort, search }, as, scrollToTop) => {
     const path = router.pathname;
     const query = router.query;
 
 
-    if(category) query.category = category;
-    if(page) query.page = page;
-    if(search) query.search = search;
-    if(sort) query.sort = sort;
+    if (category) query.category = category;
+    if (page) query.page = page;
+    if (search) query.search = search;
+    if (sort) query.sort = sort;
 
     router.push({
         pathname: path,
         query: query
-    })
+    }, as, { scroll: scrollToTop })
 }
 
 export default filterSearch
