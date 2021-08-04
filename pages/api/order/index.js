463fe30d49f9caa0c026dc7cadd072e312e6f458
MuzaffarMohammed <muzaffarmohammed43@gmail.com>
2021-08-04 17:56:22 +0530

    Issue #53 fixed - product decrement issue

--- pages/api/order/index.js
+++ pages/api/order/index.js
@@ -12,7 +12,7 @@ connectDB()
 */
 
 export default async (req, res) => {
-    switch(req.method){
+    switch (req.method) {
         case "POST":
             await createOrder(req, res)
             break;
@@ -30,28 +30,29 @@ const getOrders = async (req, res) => {
         const result = await auth(req, res)
 
         let orders;
-        if(result.role !== 'admin'){
-            orders = await Orders.find({user: result.id}).populate("user", "-password").sort({createdAt : -1})
-        }else{
-            orders = await Orders.find().populate("user", "-password").sort({createdAt : -1})
+        if (result.role !== 'admin') {
+            orders = await Orders.find({ user: result.id }).populate("user", "-password").sort({ dateOfPlaced: -1 })
+        } else {
+            orders = await Orders.find().populate("user", "-password").sort({ dateOfPlaced: -1 })
         }
 
-        res.json({orders})
+        res.json({ orders })
     } catch (err) {
-        console.log('Error occurred while getOrders: '+err);
-        return res.status(500).json({err: err.message})
+        console.log('Error occurred while getOrders: ' + err);
+        return res.status(500).json({ err: err.message })
     }
 }
 
 const createOrder = async (req, res) => {
+    console.log('Creating Order ....')
     try {
         const result = await auth(req, res)
         const { address, mobile, cart, total } = req.body
 
-        for(let i=0; i < cart.length; i++){
-            sold(cart, i)
+        for (let i = 0; i < cart.length; i++) {
+            await sold(cart, i);
         }
-        
+
         const newOrder = new Orders({
             user: result.id, address, mobile, cart, total
         })
@@ -64,40 +65,42 @@ const createOrder = async (req, res) => {
         })
 
     } catch (err) {
-        console.log('Error occurred while createOrder: '+err);
-        return res.status(500).json({err: err.message})
+        console.log('Error occurred while createOrder: ' + err);
+        return res.status(500).json({ err: err.message })
     }
 }
 
 const sold = async (cart, index) => {
-    
+
     let oldCartItem = cart[index];
     const updatedStock = oldCartItem.inStock - oldCartItem.quantity;
     const updatedSold = oldCartItem.quantity + oldCartItem.sold;
     oldCartItem.inStock = updatedStock;
     oldCartItem.sold = updatedSold;
     cart[index] = oldCartItem;
-    await Products.findOneAndUpdate({_id: oldCartItem.id}, {inStock: updatedStock, sold: updatedSold})
+    //console.log('Updating in DB cart : ', cart, 'updatedStock', updatedStock, 'updatedSold : ', updatedSold);
+    await Products.findOneAndUpdate({ _id: oldCartItem._id }, { inStock: updatedStock, sold: updatedSold })
 }
 
 const updateOrderPlaced = async (req, res) => {
     try {
         const result = await auth(req, res)
-        if(result.role === 'user'){
-            const {id, method} = req.body
-            const data = {placed: true, dateOfPlaced: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }), method : method}
-            await Orders.findOneAndUpdate({_id: id}, data)
-    
-            res.json({msg: 'Order placed successfully!',
-                    result: {
-                        placed : true,
-                        dateOfPlaced: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
-                        method : method
-                    }
+        if (result.role === 'user') {
+            const { id, method } = req.body
+            const data = { placed: true, dateOfPlaced: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }), method: method }
+            await Orders.findOneAndUpdate({ _id: id }, data)
+
+            res.json({
+                msg: 'Order placed successfully!',
+                result: {
+                    placed: true,
+                    dateOfPlaced: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
+                    method: method
+                }
             })
         }
-    }catch(err){
-        console.log('Error occurred while updateOrderPlaced: '+err);
-        return res.status(500).json({err: err.message})
+    } catch (err) {
+        console.log('Error occurred while updateOrderPlaced: ' + err);
+        return res.status(500).json({ err: err.message })
     }
 }
